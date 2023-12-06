75696a95172065032b13b884b377dae405d176dc
sumit23802380 <sumitjangid1692153@gmail.com>
2023-12-06 13:50:35 +0530

    Added : Initialized Repository

---
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollServiceMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.employeepayrollservice;
+
+/**
+ * Employee Payroll Service Main Class
+ */
+public class EmployeePayrollServiceMain {
+    public static void main(String[] args) {
+        System.out.println("Hello , Welcome to Employee Payroll Service Main Class");
+    }
+}
